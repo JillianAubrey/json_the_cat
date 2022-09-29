e96c7916843be0f45e8a54012a318ab26bfb0991
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-29 18:09:08 +0000

    Implemented command line arguments as input to the query, and an error message if no breeds are returned

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,10 +1,16 @@
 const request = require("request");
+const query = process.argv.slice(2).join('%20');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=turkish%20van', (error, response, body) => {
+console.log(query);
+
+request('https://api.thecatapi.com/v1/breeds/search?q=' + query, (error, response, body) => {
   if (error) {
     return console.log(error);
   }
   const breedInfo = JSON.parse(body);
+  if (breedInfo.length === 0) {
+    return console.log("Hmmm, can't seem to find any breed matching that in the database.")
+  }
   console.log(breedInfo);
 })
 
